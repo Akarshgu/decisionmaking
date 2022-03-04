21e791be3fb87c1ee35af1a48224da8baa99e8be
Akarshgu <akarshgupta705415>
2022-03-04 11:00:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,132 @@
+
+
+
+                         //03/03/2022
+
+
+            //decision making...
+// if ther will be 1 cond. then it will be 
+// let age =10;
+
+// if(age>18){
+//     console.log("you are 18 years old ");
+// }
+// else{
+//     console.log("you are less then 18");
+// }
+
+
+
+// if there is more than 1 thenif else ladder... will be emplemented
+
+// if else ladder
+
+//  let day=10;
+// if(day<0){
+//     console.log("sunday");
+//  }
+// else if (monday==1) {
+//     console.log("monday");
+// } 
+// else {
+//      console.log("saturday");
+// }
+
+//3 or more cond. switch case will be implemented
+
+
+// let gender="m";
+// switch(gender){
+//     case"m":
+//     console.log(male);
+//     break;
+//     // it is important to put break in switch case
+//     case"f":
+//     console.log(female);
+//     break;
+//     default:
+//     console.log(male);
+//     break;
+// }
+
+
+// Take age as the variable 
+// age < 18 = Child
+// greater than 18 but less than 40 = Adult 
+// age is greater than 40 = Old age
+
+
+
+// let age= 60;
+// switch(true){
+//       case (age < 18):
+//     console.log("child");
+//     break;
+//     case (18 < age < 40):
+//     console.log("adult");
+//     break;
+//     default:
+//     console.log("old age");
+//     break;
+// }
+
+//ques 2
+// take variable name as day , its possible values are 0 1 2 3 4 5
+// 0 = Monday 
+// 5 : Saturday
+//ans2
+// let day=0;
+// switch(day){
+//     case 0:
+//        console.log("sunday");
+//         break;
+//     case 1:
+//         console.log("monday");
+//         break;
+//     case 2:
+//          console.log("tuesday");
+//          break;
+//     case 3:
+//          console.log("wednesday");
+//          break;
+//     case 4:
+//          console.log("thursday");
+//          break;
+//     case 5:
+//          console.log("friday");
+//          break;
+//     default:
+//          console.log("saturday");
+//          break;
+// }
+
+
+// loops  for , while , do while
+//iteration statement
+//for(){}
+//for loop......
+//there will be 3 values as
+     //intialization
+     //condition
+     //increment/decrement
+
+// var a; created variable
+// declare a variable
+//var a=12;
+//we are initializing the variable with the valueof 12
+
+// for (let i = 0; i <= 10; i++) {
+//    console.log(i);  
+// }
+
+// Question : Print the table of 19 like 19 * 1 = 19
+// 19 * 1 = 19
+// 19 * 2 = 38
+
+
+// for (let i = 1; i <= 10; i++) {
+//    console.log("19*" + i +"=" + 19*i);
+// }
+
+
+
